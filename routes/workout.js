11cecfd2032f2ca7c5c91538a249d489b221cfe0
hotsoup42 <jose22gw@gmail.com>
2021-09-28 13:26:39 -0700

    created routes, for workouts,js. also  npm i nodemon

---
+++ routes/workout.js
@@ -0,0 +1,92 @@
+const router = require( 'express' ).Router();
+const db = require( '../models/workout' );
+ console.log("testing")
+// Get all the workout data and add a totalDuration field
+router.get( '/', async ( req, res ) => {
+	try {
+		const workoutsData = await db.Workout.aggregate( [
+			{
+				$addFields: {
+					totalDuration: {
+						$sum: '$exercises.duration'
+					}
+				}
+			}
+		] );
+
+		res.status( 200 ).json( workoutsData );
+
+	} catch ( err ) {
+		res.status( 400 ).json( err );
+	}
+} );
+
+// Push a new exercise to workout
+router.put( '/:id', async ( req, res ) => {
+	try {
+		const workoutData = await db.Workout.updateOne(
+			{
+				_id: req.params.id
+			},
+			{
+				$push: {
+					exercises: { ...req.body }
+				}
+			}
+		);
+
+		res.status( 200 ).json( workoutData );
+
+	} catch ( err ) {
+		res.status( 400 ).json( err );
+	}
+} );
+
+// Create workout
+router.post( '/', async ( req, res ) => {
+	try {
+		const workoutData = await db.Workout.create( req.body );
+
+		res.status( 200 ).json( workoutData );
+
+	} catch ( err ) {
+		res.status( 400 ).json( err );
+	}
+} );
+// last 7 workouts
+router.get( '/range', async ( req, res ) => {
+	try {
+		const workoutData = await db.Workout.aggregate( [
+			{
+				$sort: {
+					day: -1
+				}
+			},
+			{
+				$limit: 7
+			},
+			{
+				$addFields: {
+					totalDuration: {
+						$sum: '$exercises.duration'
+					}
+				}
+			}
+		] );
+
+		res.status( 200 ).json( workoutData );
+
+	} catch ( err ) {
+		res.status( 400 ).json( err );
+	}
+} );
+
+
+router.get( '/stats', async ( req, res ) => {
+	res.sendFile( path.join( __dirname, '/../public/stats.html' ) );
+} );
+
+router.get( '/exercise', async ( req, res ) => {
+	res.sendFile( path.join( __dirname, '/../public/exercise.html' ) );
+} );
+module.exports = router;
